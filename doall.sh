304f22e3399d673479afd6da589e8c77b9105a62
Alex Gittens <swiftset@gmail.com>
2015-06-09 11:42:41 -0700

    Update doall.sh

--- doall.sh
+++ doall.sh
@@ -21,7 +21,7 @@ sudo service apache2 stop
 sudo apt-get -y install nginx-core nginx
 cd ..
 sudo apt-get -y install upstart # for some reason this is missing!
-echo 'echo "service portmap \$1" > /etc/init.d/portmap"' | sudo bash
+echo 'echo "service portmap \$1" > /etc/init.d/portmap' | sudo bash
 sudo chmod 755  /etc/init.d/portmap
 cd /opt
 sudo rm -rf ./sge6-fresh
